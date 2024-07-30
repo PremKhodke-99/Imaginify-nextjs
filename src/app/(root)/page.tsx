707360ba12b46e5bb549ef6c73f89b38a6331ed6
Prem <premkhodke.99@gmail.com>
2024-07-30 15:16:57 +0530

    initial commit

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div>
+      <p>Home</p>
+    </div>
+  );
+}
+
+// premktechworkspace23
+// gLxxyzPskm5loeC7
+// mongodb+srv://premktechworkspace23:gLxxyzPskm5loeC7@cluster0.kmgc1fz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
---
+++ src/lib/database/models/user.model.ts
@@ -0,0 +1,56 @@
+//clerkId, email, username, photo, firstName, lastName, planId, creditBalance
+
+import { Document, model, models, Schema } from "mongoose";
+
+export interface IUser extends Document {
+    clerkId: string;
+    email: string;
+    username: string;
+    photo: string;
+    firstName: string;
+    lastName: string;
+    planId?: number; // default value is 1
+    creditBalance?: number;
+}
+
+const UserSchema = new Schema({
+    clerkId: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    photo: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    planId: {
+        type: Number,
+        default: 1,
+    },
+    creditBalance: {
+        type: Number,
+        default: 10,
+    }
+});
+
+const User = models?.User || model('User', UserSchema);
+
+export default User;
